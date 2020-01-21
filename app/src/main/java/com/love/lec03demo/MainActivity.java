1f645dd8c191c2ee954b767c34fe7ac21474fb35
300310436 <lovepreetsinghbirdi@gmail.com>
2020-01-21 10:23:05 -0800

    UI Complete

--- app/src/main/java/com/love/lec03demo/MainActivity.java
+++ app/src/main/java/com/love/lec03demo/MainActivity.java
@@ -3,12 +3,45 @@ package com.love.lec03demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView im = findViewById(R.id.imageView);
+        final Spinner sp = findViewById(R.id.spinner);
+
+
+       sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                final int index = sp.getSelectedItemPosition();
+                if(index == 0){
+                    im.setImageResource(R.drawable.concert);
+                    Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+                }
+                else if(index == 1){
+                    im.setImageResource(R.drawable.ferry);
+                    Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+                }
+                else if(index == 2){
+                    im.setImageResource(R.drawable.triathalon);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
