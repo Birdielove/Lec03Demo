ad1f4fdea35e206b7bfe5ad9bed71c2dc9bbdc08
300310436 <lovepreetsinghbirdi@gmail.com>
2020-01-21 10:38:01 -0800

    Functional

--- app/src/main/java/com/love/lec03demo/MainActivity.java
+++ app/src/main/java/com/love/lec03demo/MainActivity.java
@@ -6,20 +6,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    double price=79.99;
+    int noOfTickets;
+    double total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageView im = findViewById(R.id.imageView);
         final Spinner sp = findViewById(R.id.spinner);
-
+        final EditText tickets = findViewById(R.id.editText);
+        final Button calculate = findViewById(R.id.button);
 
        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -27,11 +31,9 @@ public class MainActivity extends AppCompatActivity {
                 final int index = sp.getSelectedItemPosition();
                 if(index == 0){
                     im.setImageResource(R.drawable.concert);
-                    Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
                 }
                 else if(index == 1){
                     im.setImageResource(R.drawable.ferry);
-                    Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
                 }
                 else if(index == 2){
                     im.setImageResource(R.drawable.triathalon);
@@ -40,8 +42,15 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
             }
         });
+       calculate.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               noOfTickets = Integer.parseInt(tickets.getText().toString());
+               total = noOfTickets * price;
+               Toast.makeText(getApplicationContext(),Double.toString(total),Toast.LENGTH_SHORT).show();
+           }
+       });
     }
 }
